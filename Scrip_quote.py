ed0f7cc019d66200a3f659c8b46bcd551bdaf5ea
Lisy29 <damaliamzn@gmail.com>
2024-07-29 18:46:06 +0200

    actualizaciòn de main

--- Scrip_quote.py
+++ Scrip_quote.py
@@ -13,6 +13,7 @@ init(autoreset=True)
 # Cargar variables de entorno desde un archivo .env
 load_dotenv()
 
+#Define una función log_to_database para registrar logs en la base de datos
 def log_to_database(connection, level, message):
     try:
         cursor = connection.cursor()
@@ -38,7 +39,7 @@ def create_connection():
     except Error as e:
         print(f"Error al conectar a MySQL: {e}")
         return None
-
+#Define la función insert_quotes para extraer y almacenar citas
 def insert_quotes():
     connection = create_connection()
     if not connection:
@@ -50,28 +51,29 @@ def insert_quotes():
         # Primer log: conexión a la base de datos establecida
         log_to_database(connection, 'INFO', "Conexión a la base de datos establecida.")
         base_url = " https://quotes.toscrape.com/page/"
-        
+        #Itera a través de las primeras 10 páginas del sitio web
         for page_num in range(1, 11):
             url = base_url + str(page_num) + '/'
             result = requests.get(url)
-            
+            #Hace una solicitud HTTP GET a la URL., busca todas las citas
             if result.status_code == 200:
                 content = result.text
                 soup = BeautifulSoup(content, 'html.parser')
                 spans = soup.find_all('span', class_='text', itemprop='text')
-
+#Si se encuentran citas, define colores para imprimir diferentes tipos de información
                 if spans:
                     yellow = Fore.YELLOW    # para las frases
                     magenta = Fore.LIGHTMAGENTA_EX    # para los autores
                     green = Fore.GREEN    # para las etiquetas
                     blue = Fore.LIGHTBLUE_EX    # para los about
-
+#Itera sobre las citas, las extrae. Encuentra el autor y extrae el about, se crea el link del about
                     for span in spans:
                         phrase = span.get_text(strip=True)
                         quote_div = span.find_parent('div', class_='quote')
                         author_name = quote_div.find('small', class_='author').get_text(strip=True)
                         about_link = quote_div.find('a', href=True)['href']
                         about_url = "https://quotes.toscrape.com" + about_link
+                       
 
                         # Insertar o obtener el autor
                         cursor.execute("SELECT id FROM authors WHERE name = %s", (author_name,))
@@ -82,7 +84,7 @@ def insert_quotes():
                             cursor.execute("INSERT INTO authors (name, about_url) VALUES (%s, %s)", (author_name, about_url))
                             connection.commit()
                             author_id = cursor.lastrowid
-
+#imprime en consola la frase, el autor, el link
                         print(yellow + "Frase extraída: " + phrase)
                         print(magenta + "Autor: " + author_name)
                         print(blue + "About link: " + about_url)
@@ -105,17 +107,21 @@ def insert_quotes():
                         tags = quote_div.find_all('a', class_='tag')
                         if tags:
                             tags_text = [tag.get_text(strip=True) for tag in tags]
+                            #imprime las etiquetas en consola
                             print(green + "Tags: " + ', '.join(tags_text))
                             log_to_database(connection, 'INFO', f"Tags: {', '.join(tags_text)}")
-
+#Inicia un bucle que itera a través de cada texto de etiqueta en la lista tag_text
                             for tag_text in tags_text:
                                 cursor.execute("SELECT id FROM tags WHERE name = %s", (tag_text,))
                                 tag = cursor.fetchone()
+#Si la etiqueta ya existe, se obtiene el id de la etiqueta existente y se almacena en tag_id
                                 if tag:
                                     tag_id = tag[0]
                                 else:
                                     cursor.execute("INSERT INTO tags (name) VALUES (%s)", (tag_text,))
+                                    #se confirma la transaccion
                                     connection.commit()
+                                    #Obtiene el id de la etiqueta recién insertada y lo almacena en tag_id
                                     tag_id = cursor.lastrowid
                                 
                                 # Verificar si la combinación de quote_id y tag_id ya existe en quote_tags
@@ -124,25 +130,30 @@ def insert_quotes():
                                 if not quote_tag:
                                     cursor.execute("INSERT INTO quote_tags (quote_id, tag_id) VALUES (%s, %s)", (quote_id, tag_id))
                                     connection.commit()
+#Esta parte se ejecuta si no se encontraron etiquetas para la frase actual
                         else:
                             print("No se encontraron etiquetas para la frase.")
                             log_to_database(connection, 'WARNING', "No se encontraron etiquetas para la frase.")
+ #Esta parte se ejecuta si no se encontraron citas en la página actual
                 else:
                     print("No se encontraron citas en la página.")
                     log_to_database(connection, 'WARNING', "No se encontraron citas en la página.")
+#Esta parte se ejecuta si la solicitud HTTP no fue exitosa 200
             else:
                 print(f"Error al realizar la solicitud: {result.status_code}")
                 log_to_database(connection, 'ERROR', f"Error al realizar la solicitud: {result.status_code}")
-
+#Captura las excepciones relacionadas con MySQL
     except Error as e:
         print(f"Error al conectar a MySQL: {e}")
         log_to_database(connection, 'ERROR', f"Error al conectar a MySQL: {e}")
-
+#Bloque que se ejecuta siempre, ya sea que ocurra una excepción o no.
+   
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            print('Conexión cerrada')
+            Rojo = Fore.RED
+            print(Rojo + 'Conexión cerrada')
             # Registrar el cierre de conexión
             # Utiliza un nuevo cursor para registrar el log ya que el cursor principal está cerrado.
             log_connection = create_connection()  # Reabre la conexión para el log final
